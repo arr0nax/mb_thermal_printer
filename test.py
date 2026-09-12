1bea90cab474750e9dc68d8b144d899e449bcac6
arr0nax <claytonccollins@gmail.com>
2026-09-12 17:04:54 -0400

    test files

---
+++ test.py
@@ -0,0 +1,20 @@
+from escpos.printer import Serial
+import os, random
+
+p = Serial(devfile='/dev/serial0', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1.00, dsrdtr=True) #initilize thermal printer serial 
+
+
+def print_random_image(cmc): #function to print image
+    path = '/home/arronax/mb_thermal_printer/images' + str(cmc) + '/'
+    try:
+        image_path = path + random.choice(os.listdir(path))
+        p.image(image_path)
+        p.textln("")
+        p.textln("")
+        p.textln("")
+    except Exception as e:
+        print("An error occurred:", e)
+
+print_random_image(0)
+
+
